ff8f7deea2b8c26fd118bffebdba46b53fab1a7a
jaiswarrvishal <163637594+jaiswarrvishal@users.noreply.github.com>
2024-05-24 18:04:26 +0530

    updte

--- day-1/src/App.js
+++ day-1/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
+import Mens from './Components/Mens';
+import Profile from './Components/Profile';
+import Register from './Components/Register';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/mens' element={<Mens />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
     </div>
   );
 }
---
+++ day-1/src/Components/Mens.jsx
@@ -0,0 +1,5 @@
+function Mens() {
+    return <div>Mens</div>;
+  }
+  
+  export default Mens;
